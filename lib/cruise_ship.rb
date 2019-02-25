c714308bbb12e02e401a07462ddec2a8ff54587e
Mackenzie Moore <macmoore@ucdavis.edu>
2019-02-25 18:57:26 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,9 +7,11 @@
 # }
 
 def select_winner(passengers)
+  winner = ""
   passengers.each do |key, value|
 if key == :suite_a && value.start_with?("A") 
-      value
+     winner = value
   end
 end
+winner
 end
