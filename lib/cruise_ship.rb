5d7af4742e056e7e06476ce626692154dc692777
Mackenzie Moore <macmoore@ucdavis.edu>
2019-02-25 19:02:26 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,5 +7,17 @@
 # }
 
 def select_winner(passengers)
-  # add the code snippet here!
+  winner = ""
+  passengers.each do |key, value|
+if key == :suite_a && value.start_with?("A") 
+     winner = value
+  end
 end
+winner
+end
+
+def happy_birthday(birthday_kids)
+birthday_kids.each do |name, age|
+puts  "Happy Birthday #{name}! You are now #{age} years old!"
+end
+end 
